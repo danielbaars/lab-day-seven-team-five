5bfd09db9db32efe03ebc359fb93ea78130936a8
Aa <stijn.aa@capgemini.com>
2023-02-09 15:41:43 +0100

    d

--- src/routes/pattern/+page.svelte
+++ src/routes/pattern/+page.svelte
@@ -1,9 +1,10 @@
 <script>
 	import '@event-calendar/core/index.css';
-	import Weekselector from '../../components/weekselector.svelte';
+	import Weekselector from '../../components/WeekSelector.svelte';
 </script>
 
 <div>
 	<h1>Pattern page</h1>
 	<Weekselector />
+	<Weekselector pattern={[2, 3, 4, 5, 6]} />
 </div>
