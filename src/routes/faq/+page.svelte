4ce60cd456a267cebd2867d96d2ff562c8f2e030
Daniel Baars <info@danielbaars.nl>
2023-02-09 14:56:14 +0100

    Add super simple store

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -1,9 +1,14 @@
 <script>
+	import AppDataStore from '../../Store.js';
 	const pageTitle = 'FAQ';
+	let siteName;
+	AppDataStore.subscribe((data) => {
+		siteName = data.siteName;
+	});
 </script>
 
 <svelte:head>
-	<title>{pageTitle}</title>
+	<title>{pageTitle} | {siteName}</title>
 	<meta name="description" content="The most commonly asked questions about Better Clarity" />
 </svelte:head>
 
