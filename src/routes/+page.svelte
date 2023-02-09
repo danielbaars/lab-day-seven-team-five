5f238509b984065a8b558fd622fbe7a6e3112526
Daniel Baars <info@danielbaars.nl>
2023-02-09 10:48:26 +0100

    Add font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<svelte:head>
+	<title>Better Clarity</title>
+	<meta name="description" content="Time tracking for the 21st century" />
+</svelte:head>
+
 <h1>Better Clarity</h1>
 <p>Time tracking for the 21st century</p>
