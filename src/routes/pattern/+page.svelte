c7b73886259169b56709c7d4d2ce51ad6d9386ae
Daniel Baars <info@danielbaars.nl>
2023-02-09 15:46:42 +0100

    Small styling fixes

--- src/routes/pattern/+page.svelte
+++ src/routes/pattern/+page.svelte
@@ -1,8 +1,15 @@
-<script>    
+<script>
 	import ClientPicker from '../../components/client-picker.svelte';
+	import HeadTitle from '../../components/HeadTitle.svelte';
+	const pageTitle = 'Pattern Builder';
 </script>
 
-<div>
-    <h1>Pattern page</h1>
-    <ClientPicker/>
-</div>
+<HeadTitle {pageTitle} />
+
+<svelte:head>
+	<title>{pageTitle}</title>
+	<meta name="description" content="Build your time tracking patterns here" />
+</svelte:head>
+
+<h1 class="page-title">{pageTitle}</h1>
+<ClientPicker />
