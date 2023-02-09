29e81a659153d81a9e3eef772da48db90febefad
Aa <stijn.aa@capgemini.com>
2023-02-09 14:41:52 +0100

    l

---
+++ src/components/weekselector.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let pattern = [8, 8, 4, 0, 0];
+</script>
+
+<div class="week-selector">
+	<ul class="week-selector__container">
+		<li class="week-selector__day">
+			<div class="week-selector__day-text">Maandag</div>
+			<div class="week-selector__day-number">{pattern[0]}</div>
+		</li>
+		<li class="week-selector__day">
+			<div class="week-selector__day-text">Dinsdag</div>
+			<div class="week-selector__day-number">{pattern[1]}</div>
+		</li>
+		<li class="week-selector__day">
+			<div class="week-selector__day-text">Woensdag</div>
+			<div class="week-selector__day-number">{pattern[2]}</div>
+		</li>
+		<li class="week-selector__day">
+			<div class="week-selector__day-text">Donderdag</div>
+			<div class="week-selector__day-number">{pattern[3]}</div>
+		</li>
+		<li class="week-selector__day">
+			<div class="week-selector__day-text">Vrijdag</div>
+			<div class="week-selector__day-number">{pattern[4]}</div>
+		</li>
+	</ul>
+</div>
+
+<style lang="scss">
+</style>
--- src/routes/pattern/+page.svelte
+++ src/routes/pattern/+page.svelte
@@ -1,7 +1,9 @@
 <script>
-    import '@event-calendar/core/index.css';
+	import '@event-calendar/core/index.css';
+	import Weekselector from '../../components/weekselector.svelte';
 </script>
 
 <div>
-    <h1>Pattern page</h1>
+	<h1>Pattern page</h1>
+	<Weekselector />
 </div>
